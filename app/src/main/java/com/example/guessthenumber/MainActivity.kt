dc30aa1a7de8719239fe27c80e26ceddd3860178
Emma Canini <emma.canini@btconnect.com>
2024-07-11 13:09:21 +0100

    UI updated with button functionality

--- app/src/main/java/com/example/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,10 +24,14 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.guessthenumber.ui.theme.GuessTheNumberTheme
 
@@ -33,8 +43,8 @@ class MainActivity : ComponentActivity() {
             GuessTheNumberTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     appScreen()
-                    GuessButton()
-                    ModifiableTextField()
+
+
                     
                 }
             }
@@ -42,20 +52,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
 
 @Composable
 @Preview(showBackground = true)
 fun appScreen() {
     Header()
     Body()
-    GuessButton()
+
         
     }
 
@@ -64,21 +67,49 @@ fun appScreen() {
 fun Header() {
     val headerModifier = Modifier.fillMaxSize()
     Box(modifier = headerModifier) {
-        Text(text = "GUESSTHENUMBER",
-            fontSize = 46.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
+
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Mysical Logo",
+                    modifier = Modifier.fillMaxSize()
         )
 
+        Text(text = "GUESS THE NUMBER",
+            fontSize = 46.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.height(40.dp)
+        )
+
+
+
+//        Text(text = "Correct/Incorrect depending on user input",
+//            fontSize = 46.sp,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier.height(40.dp)
+//        )
+
     }
+
 }
-
-
 
 @Composable
 fun Body() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(40.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ModifiableTextField()
+            Spacer(modifier = Modifier.height(20.dp))
+            GuessButton()
+            Spacer(modifier = Modifier.height(450.dp))
+            GenerateNumberButton()
+        }}
 
 }
+
 
 
 @Composable
@@ -87,7 +118,14 @@ fun ModifiableTextField() {
         mutableStateOf("")
     }
     TextField(value = currentText,
-        onValueChange = { newValue -> currentText = newValue }
+        onValueChange = { newValue -> currentText = newValue },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = { Text(text = "type number...") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+
+
     )
 }
 
@@ -95,10 +133,23 @@ fun ModifiableTextField() {
 @Composable
 fun GuessButton() {
     Button(onClick= {
-        println("Guess submitted")
+       println("Guess submitted")
     }){
         Text(
             text= stringResource(R.string.button_label)
+        )
+    }
+}
+
+@Composable
+fun GenerateNumberButton() {
+    Button(onClick= {
+        val randomNumber = (0..100).random()
+        println(randomNumber)
+
+    }){
+        Text(
+            text= stringResource(R.string.button_label2)
         )
     }
 }
